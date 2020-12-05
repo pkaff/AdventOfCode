8a8a839f183c4d0fb1fa478172d92ff6681021ef
Pkaff <wishing@live.se>
2020-12-05 10:25:49 +0100

    add oneline for day5 part2

--- 2020/Day5/part2.py
+++ 2020/Day5/part2.py
@@ -1,7 +1,2 @@
 myIDs = [int(rowcol[:-3], 2) * 8 + int(rowcol[-3:], 2) for rowcol in [''.join(['1' if (c == "B" or c == "R") else '0' for c in l.rstrip()]) for l in open("input.txt", "r").readlines()]]
-for row in range(1, 128 - 1):
-    for col in range(8):
-        id = row * 8 + col
-        if id not in myIDs and id - 1 in myIDs and id + 1 in myIDs:
-            print(id)
-            exit()
+print(int([row * 8 + col for col in range(8) for row in range(1, 128 - 1) if row * 8 + col not in myIDs and row * 8 + col - 1 in myIDs and row * 8 + col + 1 in myIDs]))
