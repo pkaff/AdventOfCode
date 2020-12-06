0d3694c1c08af8f3473b129ca82a735211a8cf96
Pkaff <wishing@live.se>
2020-12-06 14:52:59 +0100

    Update day6 solutions to oneliners

--- 2020/Day6/part1.py
+++ 2020/Day6/part1.py
@@ -1,9 +1,1 @@
-declarations = [dict()]
-myinput = [l.strip() for l in open("input.txt", "r").readlines()]
-for line in myinput:
-    if not line:
-        declarations.append(dict())
-    else:
-        for q in line:
-            declarations[-1][q] = True
-print(sum([len(groupDecl) for groupDecl in declarations]))
+print(sum([len(set("".join(l.split("\n")))) for l in open("input.txt", "r").read().split("\n\n")]))
