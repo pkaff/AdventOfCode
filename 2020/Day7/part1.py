59b4f45662fb7a8ad16589a3aa9672440388df25
Pkaff <wishing@live.se>
2020-12-08 08:49:23 +0100

    cleanup day7 2020

--- 2020/Day7/part1.py
+++ 2020/Day7/part1.py
@@ -2,15 +2,12 @@ from collections import defaultdict
 import queue
 
 myinput = [l.rstrip("\n.") for l in open("input.txt", "r").readlines()]
-adjList = defaultdict(list)
 revAdjList = defaultdict(list)
 for l in myinput:
     outerBag, l2 = l.split("s contain ")
     innerBags = l2.split(", ")
     for innerLine in innerBags:
-        nBags = innerLine[0]
         innerBagColor = innerLine[2:].rstrip("s")
-        adjList[outerBag].append((nBags, innerBagColor))
         revAdjList[innerBagColor].append(outerBag)
 
 q = queue.Queue()
--- 2020/Day7/part2.py
+++ 2020/Day7/part2.py
@@ -3,7 +3,6 @@ import queue
 
 myinput = [l.rstrip("\n.") for l in open("input.txt", "r").readlines()]
 adjList = defaultdict(list)
-revAdjList = defaultdict(list)
 for l in myinput:
     outerBag, l2 = l.split("s contain ")
     innerBags = l2.split(", ")
@@ -13,7 +12,6 @@ for l in myinput:
             continue
         innerBagColor = innerLine[2:].rstrip("s")
         adjList[outerBag].append((int(nBags), innerBagColor))
-        revAdjList[innerBagColor].append(outerBag)
 
 def DFS(adjList, bag, nCurrentBags):
     if not adjList[bag]:
