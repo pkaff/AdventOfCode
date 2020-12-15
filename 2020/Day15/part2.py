d87b606802e1c0154d26154d314acbe0f885760b
Pkaff <wishing@live.se>
2020-12-15 19:38:40 +0100

    cleanup day15 solution

--- 2020/Day15/part2.py
+++ 2020/Day15/part2.py
@@ -1,25 +1,17 @@
-def putToMap(mymap, lastNbr, ix):
-    if lastNbr in mymap:
-        mymap[lastNbr][1] += 1
-    else:
-        mymap[lastNbr] = [ix, 1]
 myinput = [int(val) for val in open("input.txt", "r").readline().rstrip("\n").split(",")]
 lastNbr = 0
 mymap = {}
 for ix in range(30000000):
     if ix < len(myinput):
         lastNbr = myinput[ix]
-        putToMap(mymap, lastNbr, ix)
+        mymap[lastNbr] = ix
     else:
-        if lastNbr in mymap and mymap[lastNbr][1] > 1:
-            lastIx = mymap[lastNbr][0]
-            mymap[lastNbr][0] = ix - 1
-            lastNbr = mymap[lastNbr][0] - lastIx
-            putToMap(mymap, lastNbr, ix)
-        elif lastNbr in mymap:
-            lastNbr = 0
-            putToMap(mymap, lastNbr, ix)
+        if lastNbr not in mymap:
+            mymap[lastNbr] = ix - 1
+        if mymap[lastNbr] < ix - 1:
+            tmp = lastNbr
+            lastNbr = ix - 1 - mymap[lastNbr]
+            mymap[tmp] = ix - 1
         else:
             lastNbr = 0
-            putToMap(mymap, lastNbr, ix)
 print(lastNbr)
