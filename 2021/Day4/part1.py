2b1f9456863d15680422b060bd55789bd9834e3d
Pkaff <wishing@live.se>
2021-12-05 11:30:11 +0100

    Add day4 solutions

---
+++ 2021/Day4/part1.py
@@ -0,0 +1,11 @@
+myin = open("input.txt", "r").read().split("\n\n")
+draw_numbers = list(map(int, myin[0].split(',')))
+boards = [[list(map(int, line.split())) for line in board.split('\n')] for board in myin[1:]]
+for num in draw_numbers:
+    for (ix, board) in enumerate(boards):
+        boards[ix] = [[-1 if num == board_num else board_num for board_num in line] for line in board]
+        row_win = any(list(map(lambda line: all(board_num == -1 for board_num in line), boards[ix])))
+        col_win = any(list(map(lambda line: all(board_num == -1 for board_num in line), list(zip(*boards[ix])))))
+        if row_win or col_win:
+            print(sum(list(map(lambda line: sum([board_num if board_num != -1 else 0 for board_num in line]), boards[ix]))) * num)
+            exit()
