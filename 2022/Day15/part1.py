d0754703788526c77484f2449e2751c5ecdc2e91
Pkaff <wishing@live.se>
2022-12-15 21:10:14 +0100

    Add day 15 part1 unoptimized

--- 2022/Day15/part1.py
+++ 2022/Day15/part1.py
@@ -12,15 +12,44 @@ input = [((lst[0], lst[1]), (lst[2], lst[3])) for lst in input]
 sensors = [lst[0] for lst in input]
 beacons = [lst[1] for lst in input]
 
-blocked_x_ranges = defaultdict(set)
+class Intervals:
+    def __init__(self):
+        self.ranges = []
+        self.ix = 0
+    def merge(self):
+        for ix, (r1, r2) in enumerate(zip(self.ranges[1:], self.ranges[:-1])):
+            if max(r1.start, r2.start) <= min(r1.stop, r2.stop):
+                self.ranges[ix] = range(min(r1.start, r2.start), max(r1.stop, r2.stop))
+                del self.ranges[ix + 1]
+                return True
+        return False
+    def append(self, r):
+        self.ranges.append(r)
+        self.ranges.sort(key=lambda r: r.start)
+
+        while self.merge():
+            pass
+    def count(self, beacons):
+        count = 0
+        for r in self.ranges:
+            count += r.stop - r.start
+            count -= sum([1 for pos in beacons if pos in r])
+        return count
+    def __str__(self):
+        return str(self.ranges)
+    def __repr__(self):
+        return str(self)
+
+row_to_check = 2000000
+
+blocked_x_ranges = defaultdict(Intervals)
 for sensor, beacon in zip(sensors, beacons):
     dist = manhattan_dist(sensor, beacon)
     for dy in range(-dist, dist + 1):
-        start = -abs(abs(dy) - dist)
-        stop = abs(abs(dy) - dist) + 1
-        blocked_x_ranges[sensor[1] + dy].add(range(sensor[0] + start, sensor[0] + stop))
+        if sensor[1] + dy == row_to_check:
+            start = -abs(abs(dy) - dist)
+            stop = abs(abs(dy) - dist) + 1
+            blocked_x_ranges[sensor[1] + dy].append(range(sensor[0] + start, sensor[0] + stop))
 
-col_to_check = 2000000
-blocked_pos = set(chain(*blocked_x_ranges[col_to_check]))
-beacons_in_blocked_pos = sum([1 for x in blocked_pos if (x, col_to_check) in beacons])
-print(len(blocked_pos) - beacons_in_blocked_pos)
+beacons_in_row = [beacon[0] for beacon in set(beacons) if beacon[1] == row_to_check]
+print(blocked_x_ranges[row_to_check].count(beacons_in_row))
