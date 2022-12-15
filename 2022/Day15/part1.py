080d842dc8d0337135936277af5ac75abb87f1d4
Daniel Odenbrand <daniel.odenbrand@volvocars.com>
2022-12-15 16:31:48 +0100

    Continue WIP day 15

--- 2022/Day15/part1.py
+++ 2022/Day15/part1.py
@@ -7,22 +7,20 @@ def manhattan_dist(sensor, beacon):
     xx, yy = beacon
     return abs(x - xx) + abs(y - yy)
 
-input = [list(map(int, re.findall(r'-?\d+', line.strip('\n')))) for line in open("testinput.txt", "r").readlines()]
+input = [list(map(int, re.findall(r'-?\d+', line.strip('\n')))) for line in open("input.txt", "r").readlines()]
 input = [((lst[0], lst[1]), (lst[2], lst[3])) for lst in input]
+sensors = [lst[0] for lst in input]
+beacons = [lst[1] for lst in input]
 
 blocked_x_ranges = defaultdict(set)
-for sensor, beacon in input:
+for sensor, beacon in zip(sensors, beacons):
     dist = manhattan_dist(sensor, beacon)
     for dy in range(-dist, dist + 1):
-        start, stop = 0, 0
-        start = -(abs(abs(dy) - dist))
+        start = -abs(abs(dy) - dist)
         stop = abs(abs(dy) - dist) + 1
-        if beacon[1] == dy:
-            if sensor[0] - beacon[0] < 0:
-                start += 1
-            else:
-                stop -= 1
-        blocked_x_ranges[sensor[1] + dy].add(range(start, stop))
+        blocked_x_ranges[sensor[1] + dy].add(range(sensor[0] + start, sensor[0] + stop))
 
-print(blocked_x_ranges[10])
-print(len(set(chain(*blocked_x_ranges[10]))))
+col_to_check = 2000000
+blocked_pos = set(chain(*blocked_x_ranges[col_to_check]))
+beacons_in_blocked_pos = sum([1 for x in blocked_pos if (x, col_to_check) in beacons])
+print(len(blocked_pos) - beacons_in_blocked_pos)
