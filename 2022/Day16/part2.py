6914c22da8766163d29a2528c6b6380edf22e104
Daniel Odenbrand <daniel.odenbrand@volvocars.com>
2022-12-16 14:49:55 +0100

    Add 2022 day16 part2

--- 2022/Day16/part2.py
+++ 2022/Day16/part2.py
@@ -1,33 +1,35 @@
 from collections import defaultdict
 import functools
+import itertools
+import sys
 input = [line.strip('\n').split('; ') for line in open("input.txt", "r").readlines()]
-flow_rates = defaultdict(int)
-adj_list = defaultdict(list)
+flow_rates = dict()
+dists = defaultdict(lambda: sys.maxsize)
+nodes = set()
 for line in input:
     label = line[0].split("Valve ")[1][:2]
+    nodes.add(label)
     flow_rate = int(line[0].split('=')[1])
-    flow_rates[label] = flow_rate
+    if flow_rate != 0:
+        flow_rates[label] = flow_rate
     connected_tunnels = [tunnel.strip(',') for tunnel in line[1].split()[4:]]
     for tunnel in connected_tunnels:
-        adj_list[label].append(tunnel)
+        dists[tunnel, label] = 1
 
-print(flow_rates)
-print(adj_list)
+#floyd-warshall for min distances
+for k, i, j in itertools.product(nodes, nodes, nodes):
+    dists[i, j] = min(dists[i, j], dists[i, k] + dists[k, j])
 
 @functools.lru_cache(maxsize=None)
-def tunnel_recursion(pos, time_left, opened):
-    if time_left <= 0:
-        return 0
-    mypos, elepos = pos
-    max_flow = 0
-    added_flow = 0
-    if pos not in opened:
-        added_flow = flow_rates[pos] * (time_left - 1)
-        new_opened = opened + (pos,)
-    for neighbour in adj_list[pos]:
-        if added_flow != 0:
-            max_flow = max(max_flow, added_flow + tunnel_recursion(neighbour, time_left - 2, new_opened))
-        max_flow = max(max_flow, tunnel_recursion(neighbour, time_left - 1, opened))
-    return max_flow
+def tunnel_recursion(time_left, pos, to_visit, first):
+    flows = []
+    for neighbour in to_visit:
+        if dists[pos, neighbour] >= time_left:
+            continue
+        next_time = time_left - dists[pos, neighbour] - 1
+        added_flow = flow_rates[neighbour] * next_time
+        flows.append(added_flow + tunnel_recursion(next_time, neighbour, to_visit-{neighbour}, first))
+    flows.append(tunnel_recursion(26, 'AA', to_visit, False) if first else 0)
+    return max(flows)
 
-print(tunnel_recursion(('AA', 'AA'), 26, tuple()))
+print(tunnel_recursion(26, 'AA', frozenset(flow_rates), True))
