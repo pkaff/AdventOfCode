ae74eb5ed56e23a3de10e78ca6a96d7801ede6f7
Daniel Vinnlund <dvinnlund@apple.com>
2024-12-12 09:05:39 +0100

    Add 2024 day12 part2 solutions

--- 2024/Day12/part2.py
+++ 2024/Day12/part2.py
@@ -1,21 +1,53 @@
 garden = {(x, y): c for y, row in enumerate(open("input.txt", "r").readlines()) for x, c in enumerate(row.strip())}
 max_x = max(garden.keys(), key=lambda c: c[0])[0]
 max_y = max(garden.keys(), key=lambda c: c[1])[1]
+for x in [-1, max_x + 1]:
+    for y in range(-1, max_y + 2):
+        garden[(x, y)] = '.'
+for y in [-1, max_y + 1]:
+    for x in range(-1, max_x + 2):
+        garden[(x, y)] = '.'
+
 visited = set()
 cost = 0
-def point_in_bounds_and_same_plot(pos, c):
-    return pos[0] >= 0 and pos[0] <= max_x and pos[1] >= 0 and pos[1] <= max_y and c == garden[pos]
 
 def get_neighbours(pos, c):
     dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    return [(pos[0] + dir[0], pos[1] + dir[1]) for dir in dirs if point_in_bounds_and_same_plot((pos[0] + dir[0], pos[1] + dir[1]), c)]
+    return [add(pos, dir) for dir in dirs if garden[add(pos, dir)] == c]
+
+def add(pos, dir):
+    return (pos[0] + dir[0], pos[1] + dir[1])
+
+def count_corners(pos, c):
+    corners = 0
+    # normal corners
+    if garden[add(pos, (0, -1))] != c and garden[add(pos, (1, 0))] != c:
+        corners += 1
+    if garden[add(pos, (0, -1))] != c and garden[add(pos, (-1, 0))] != c:
+        corners += 1
+    if garden[add(pos, (0, 1))] != c and garden[add(pos, (1, 0))] != c:
+        corners += 1
+    if garden[add(pos, (0, 1))] != c and garden[add(pos, (-1, 0))] != c:
+        corners += 1
+
+    # 'negative' corners
+    if garden[add(pos, (0, -1))] == c and garden[add(pos, (-1, 0))] == c and garden[add(pos, (-1, -1))] != c:
+        corners += 1
+    if garden[add(pos, (0, -1))] == c and garden[add(pos, (1, 0))] == c and garden[add(pos, (1, -1))] != c:
+        corners += 1
+    if garden[add(pos, (0, 1))] == c and garden[add(pos, (-1, 0))] == c and garden[add(pos, (-1, 1))] != c:
+        corners += 1
+    if garden[add(pos, (0, 1))] == c and garden[add(pos, (1, 0))] == c and garden[add(pos, (1, 1))] != c:
+        corners += 1
+
+    return corners
 
 for x in range(max_x + 1):
     for y in range(max_y + 1):
         if (x, y) in visited:
             continue
         area = 0
-        perimiter = 0
+        corners = 0
         to_visit = [(x, y)]
         c = garden[(x, y)]
         while to_visit:
@@ -25,7 +57,7 @@ for x in range(max_x + 1):
             visited.add(pos)
             area += 1
             neighbours = get_neighbours(pos, c)
-            perimiter += 4 - len(neighbours)
+            corners += count_corners(pos, c)
             to_visit.extend(neighbours)
-        cost += area * perimiter
+        cost += area * corners
 print(cost)
