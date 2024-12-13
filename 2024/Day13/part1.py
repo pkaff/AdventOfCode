30f29e7edac88c9aed7dd729b9d3f74975144dd4
Daniel Vinnlund <dvinnlund@apple.com>
2024-12-13 08:56:29 +0100

    Add 2024 day13 solutions

---
+++ 2024/Day13/part1.py
@@ -0,0 +1,18 @@
+import string
+import numpy as np
+inputs = [section.split("\n") for section in open("input.txt", "r").read().split("\n\n")]
+a_cost = 3
+b_cost = 1
+res = 0
+for section in inputs:
+    xa, ya = list(map(lambda x: int(x.strip(string.ascii_letters).strip(string.punctuation)), section[0].split()[2:]))
+    xb, yb = list(map(lambda x: int(x.strip(string.ascii_letters).strip(string.punctuation)), section[1].split()[2:]))
+    target_x, target_y = list(map(lambda x: int(x.strip(string.ascii_letters).strip(string.punctuation)), section[2].split()[1:]))
+    A = np.array([[xa, xb], [ya, yb]])
+    B = np.array([[target_x], [target_y]])
+    solution = np.linalg.solve(A, B)
+    sol_a = round(solution[0][0])
+    sol_b = round(solution[1][0])
+    if sol_a * xa + sol_b * xb == target_x and sol_a * ya + sol_b * yb == target_y:
+        res += a_cost * round(solution[0][0]) + b_cost * round(solution[1][0])
+print(res)
