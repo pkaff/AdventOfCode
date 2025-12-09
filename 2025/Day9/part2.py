1a0f8e7d2e687051e1da27fb30be5728d0a6297a
Daniel Vinnlund <daniel.vinnlund@se.com>
2025-12-09 12:53:37 +0100

    Add 2025 Day9 part2 solution

--- 2025/Day9/part2.py
+++ 2025/Day9/part2.py
@@ -1,21 +1,17 @@
-from itertools import combinations
-from sympy import Point, Segment, Polygon
-coords = [Point(list(map(int, line.strip().split(',')))) for line in open("testinput.txt", "r").readlines()]
-combs = combinations(coords, 2)
-rects = [Polygon(p1, Point(p1.x, p2.y), p2, Point(p2.x, p1.y)) for p1, p2 in combs]
-rects = sorted(rects, key=lambda rect: rect.area if isinstance(rect, Polygon) else 0, reverse=True)
-borders = [Segment(p[0], p[1]) for p in zip(coords[:-1], coords[1:])] + [Segment(coords[-1], coords[0])]
-for rect in rects:
-    valid = True
-    for border in borders:
-        intersect = rect.intersection(border)
-        assert(len(intersect) <= 1)
-        if not intersect:
-            continue
-        ipoint = intersect[0]
-        if isinstance(ipoint, Point) and (ipoint.x != border.p1.x or ipoint.y != border.p1.y) and (ipoint.x != border.p2.x or ipoint.y != border.p2.y):
-            valid = False
-            break
-    if valid:
-        print(rect.area)
-        break
+from itertools import combinations, pairwise
+coords = [list(map(int, line.strip().split(','))) for line in open("input.txt", "r").readlines()]
+def sorted_boundaries(boundaries):
+    return [(min(p1[0], p2[0]), min(p1[1], p2[1]), max(p1[0], p2[0]), max(p1[1], p2[1])) for p1, p2 in boundaries]
+combs = sorted_boundaries(combinations(coords, 2))
+borders = sorted_boundaries(pairwise(coords))
+maxSize = 0
+for minx, miny, maxx, maxy in combs:
+    size = (maxx - minx + 1) * (maxy - miny + 1)
+    if size > maxSize:
+        valid = True
+        for bminx, bminy, bmaxx, bmaxy in borders:
+            if bminx < maxx and bminy < maxy and bmaxx > minx and bmaxy > miny:
+                valid = False
+        if valid:
+            maxSize = size
+print(maxSize)
