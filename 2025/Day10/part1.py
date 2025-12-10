47c1d57598d243e39c935ab481b283fcced602f4
Daniel Vinnlund <daniel.vinnlund@se.com>
2025-12-10 10:08:34 +0100

    Add 2025 day10 updated solutions

--- 2025/Day10/part1.py
+++ 2025/Day10/part1.py
@@ -1,29 +1,32 @@
-from functools import lru_cache
 from ast import literal_eval
 import sys
-from frozendict import frozendict
+from collections import defaultdict
 
-sys.setrecursionlimit(10**6)
-input = [line.strip().split() for line in open("testinput.txt", "r").readlines()]
-def count_presses(target, buttons, bests, cur, num_presses):
-    if cur == target:
-        return num_presses
-    if cur in bests:
-        return bests[cur]
-    new_bests = dict(bests)
-    new_bests[cur] = num_presses
-
-    min_presses = sys.maxsize
-    for button in buttons:
+input = [line.strip().split() for line in open("input.txt", "r").readlines()]
+def count_presses(target, buttons):
+    bests = defaultdict(lambda: sys.maxsize)
+    to_visit = [(button, 1, '.'*len(target)) for button in buttons]
+    visited = set()
+    visited.add('.'*len(target))
+    while to_visit:
+        button, num_presses, cur = to_visit.pop(0)
         new_cur = list(cur)
         for strix in button:
             new_cur[strix] = '.' if new_cur[strix] != '.' else '#'
-        min_presses = min(count_presses(target, buttons, frozendict(new_bests), ''.join(new_cur), num_presses + 1), min_presses)
-    return min_presses
-    
+        new_cur_str = ''.join(new_cur)
+        if new_cur_str in visited:
+            continue
+        visited.add(new_cur_str)
+        bests[new_cur_str] = min(bests[new_cur_str], num_presses)
+        if new_cur_str == target:
+            return num_presses
+        for next_button in buttons:
+            to_visit.append((next_button, num_presses + 1, new_cur_str))
+    return bests[target]
+
 presses = 0
 for row in input:
     goal = row[0][1:-1]
     buttons = tuple(literal_eval(t) if len(t) > 3 else (literal_eval(t),) for t in row[1:-1])
-    presses += count_presses(goal, buttons, frozendict(), '.' * len(goal), 0)
+    presses += count_presses(goal, buttons)
 print(presses)
--- 2025/Day10/part2.py
+++ 2025/Day10/part2.py
@@ -1,17 +1,26 @@
-from itertools import combinations, pairwise
-coords = [list(map(int, line.strip().split(','))) for line in open("input.txt", "r").readlines()]
-def sorted_boundaries(boundaries):
-    return [(min(p1[0], p2[0]), min(p1[1], p2[1]), max(p1[0], p2[0]), max(p1[1], p2[1])) for p1, p2 in boundaries]
-combs = sorted_boundaries(combinations(coords, 2))
-borders = sorted_boundaries(pairwise(coords))
-maxSize = 0
-for minx, miny, maxx, maxy in combs:
-    size = (maxx - minx + 1) * (maxy - miny + 1)
-    if size > maxSize:
-        valid = True
-        for bminx, bminy, bmaxx, bmaxy in borders:
-            if bminx < maxx and bminy < maxy and bmaxx > minx and bmaxy > miny:
-                valid = False
-        if valid:
-            maxSize = size
-print(maxSize)
+from ast import literal_eval
+import numpy as np
+from scipy.optimize import LinearConstraint
+from scipy.optimize import milp
+
+input = [line.strip().split() for line in open("input.txt", "r").readlines()]
+
+def count_presses(buttons, joltage):
+    c = np.array([1] * len(buttons))
+    a = [[0] * len(buttons) for _ in range(len(joltage))]
+    for bidx, button in enumerate(buttons):
+        for swix in button:
+            a[swix][bidx] = 1
+    A = np.array(a)
+    b_u = np.array(joltage)
+    b_l = b_u
+    constraints = LinearConstraint(A, b_l, b_u)
+    integrality = np.ones_like(c)
+    res = milp(c=c, constraints=constraints, integrality=integrality)
+    return res.fun
+presses = 0
+for row in input:
+    buttons = [literal_eval(t) if len(t) > 3 else (literal_eval(t),) for t in row[1:-1]]
+    joltage = [int(x) for x in row[-1][1:-1].split(',')]
+    presses += count_presses(buttons, joltage)
+print(presses)
