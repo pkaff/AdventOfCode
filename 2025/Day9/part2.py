c5880ef7e2c785f00726b8c9e6d7f1ec87e18ddb
Daniel Vinnlund <daniel.vinnlund@se.com>
2025-12-10 08:10:29 +0100

    Optimize 2025 day9

--- 2025/Day9/part2.py
+++ 2025/Day9/part2.py
@@ -1,7 +1,7 @@
 from itertools import combinations, pairwise
 coords = [list(map(int, line.strip().split(','))) for line in open("input.txt", "r").readlines()]
 def sorted_boundaries(boundaries):
-    return [(min(p1[0], p2[0]), min(p1[1], p2[1]), max(p1[0], p2[0]), max(p1[1], p2[1])) for p1, p2 in boundaries]
+    return sorted([(min(p1[0], p2[0]), min(p1[1], p2[1]), max(p1[0], p2[0]), max(p1[1], p2[1])) for p1, p2 in boundaries])
 combs = sorted_boundaries(combinations(coords, 2))
 borders = sorted_boundaries(pairwise(coords))
 maxSize = 0
@@ -12,6 +12,7 @@ for minx, miny, maxx, maxy in combs:
         for bminx, bminy, bmaxx, bmaxy in borders:
             if bminx < maxx and bminy < maxy and bmaxx > minx and bmaxy > miny:
                 valid = False
+                break
         if valid:
             maxSize = size
 print(maxSize)
