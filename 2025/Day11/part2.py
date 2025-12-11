0fbd9a4156a939fbf9283e525e90cb7c6d514b0c
Daniel Vinnlund <daniel.vinnlund@se.com>
2025-12-11 09:30:25 +0100

    Add 2025 day11 part2 solution

--- 2025/Day11/part2.py
+++ 2025/Day11/part2.py
@@ -1,45 +1,15 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-input = {line.split(':')[0]: line.split(':')[1].strip().split() for line in open("input.txt", "r").readlines()}
-start = 'svr'
-end = 'out'
-G = nx.DiGraph(input)
-# color_map = []
-# for node in G:
-#     if str(node) == 'svr' or str(node) == 'out':
-#         color_map.append('green')
-#     elif str(node) == 'fft' or str(node) == 'dac':
-#         color_map.append('red')
-#     else:
-#         color_map.append('blue')
-# nx.draw(G, node_color=color_map, with_labels=True)
-# plt.savefig("graph.png", dpi=1000)
-# plt.show()
-# exit()
-# print(input)
-# valid_paths = 0
-# for path in nx.all_simple_paths(G, source='svr', target='out'):
-#     valid_paths += 1
-# print(valid_paths)
-paths = []
-for to_visit in ['fft', 'dac']:
-    desc = nx.descendants(G, to_visit)
-    anc = nx.ancestors(G, to_visit)
-
-    paths.extend([path for p in desc for path in nx.all_simple_paths(G, to_visit, p)])
-    paths.extend([path for p in anc for path in nx.all_simple_paths(G, p, to_visit)])
-
-def remove_edges(G, node=0):
-  desc = nx.descendants(G, node)
-  anc = nx.ancestors(G, node)
-  nodes = anc.union(desc).union({node})
-  Q = nx.DiGraph()
-  for i, j in G.edges:
-    # remove edges with either source or target not in nodes
-    if (i not in nodes) or (j not in nodes):
-      continue
-    # remove edges immediately connecting ancestors to descendants
-    if (i in anc) and (j in desc):
-      continue
-    Q.add_edge(i,j)
-  return Q  
+from functools import lru_cache
+from math import prod
+from collections import defaultdict
+input = defaultdict(set)
+for line in open("input.txt", "r").readlines():
+    input[line.split(':')[0]].update(line.split(':')[1].strip().split())
+@lru_cache(None)
+def DFS(start, end):
+    if end == start:
+        return 0
+    if end in input[start]:
+        return 1
+    else:
+        return sum([DFS(neighbour, end) for neighbour in input[start]])
+print(prod([DFS('svr', 'fft'), DFS('fft', 'dac'), DFS('dac', 'out')]) + prod([DFS('svr', 'dac'), DFS('dac', 'fft'), DFS('fft', 'out')]))
